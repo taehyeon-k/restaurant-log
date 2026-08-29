5889575e6203d2d66526d55068ba351b3a267545
Taehyeon Kim <100190715+taehyeon-k@users.noreply.github.com>
2026-08-29 23:02:36 +0900

    Update page.tsx

--- src/app/add/page.tsx
+++ src/app/add/page.tsx
@@ -1,5 +1,10 @@
+import { Suspense } from "react";
 import RecordForm from "../_components/RecordForm";
 
 export default function AddRestaurantPage() {
-  return <RecordForm />;
+  return (
+    <Suspense fallback={null}>
+      <RecordForm />
+    </Suspense>
+  );
 }
